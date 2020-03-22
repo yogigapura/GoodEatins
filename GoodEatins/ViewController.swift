a629b906efb7ff0f1b1ee9fd9aaf8501bc612593
yogi <yogi.gapura@gmail.com>
2020-03-22 13:38:26 +0700

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by yogi on 22/03/20.
+//  Copyright © 2020 ardiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
